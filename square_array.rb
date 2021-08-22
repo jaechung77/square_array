4d7ed833ba9eafbb4962ce2e77ec3aded7fc0339
jaechung77 <jaechung77@gmail.com>
2021-08-22 22:48:49 +1000

    lab

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  new_numbers = []
+  index = 0 
+  numbers.each do |number|
+    new_numbers[index] = number * number
+    index += 1
+  end
+  return new_numbers  
 end
